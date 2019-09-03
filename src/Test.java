ecb2e0f55d9616c2d29d28ef7ff9ef6e66ce6df7
land1908e <land1908e@gmail.com>
2019-09-03 20:21:48 +0700

    conden

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hien thi dong 1!!!!");
+        System.out.println("Hien thi dong 2!!!!");
+        System.out.println("Hien thi dong 3!!!!");
+        System.out.println("Hien thi dong 4!!!!");
+        System.out.println("Hien thi dong 5!!!!");
+        System.out.println("Hien thi dong 6!!!!");
+    }
+}
